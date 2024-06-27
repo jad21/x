db816338b705d97f3e49e564fac8ac9be92813dc
Jose Delgado <jose.delgado@maujagroup.com>
2024-06-26 21:33:52 -0400

    IntToString

---
+++ converters.go
@@ -0,0 +1,19 @@
+package hgo
+
+import "fmt"
+
+func Int64ToString(v int64) string {
+	return fmt.Sprintf("%d", v)
+}
+
+func UInt64ToString(v uint64) string {
+	return fmt.Sprintf("%d", v)
+}
+
+func IntToString(v int) string {
+	return fmt.Sprintf("%d", v)
+}
+
+func UIntToString(v uint) string {
+	return fmt.Sprintf("%d", v)
+}
